3e40f2ec660fbc934fcb59af1b3d6b0a1436fa4c
kari <iwanfadill37@gmail.com>
2025-06-10 19:19:06 +0800

    0.6.3 | Home Revamp

--- components.js
+++ components.js
@@ -4,13 +4,13 @@ class THeader extends HTMLElement {
     this.innerHTML = `
       <div class="headcontain sticky">
         <header class="nosel ">
-        <a href="/index.html"><img src="/assets/img/logo.png" alt="Logo Holyverse" class="logo"></a>
+        <a href="/index.html"><img src="/assets/img/logo.webp" alt="Logo Holyverse" class="logo"></a>
         <a href="/index.html"><img src="/favicon.ico" alt="Logo Holyverse" class="logosm"></a>
         <nav>
             <a href="/list.html" class="navlink">List Negara</a>
             <a href="/news.html" class="navlink">Berita</a>
             <a href="https://hvtime.krizknt.icu/" class="navlink">Waktu</a>
-            <a href="" class="navbtn navlink">Join</a>
+            <a href="https://discord.gg/tdCVJf9G" class="navbtn navlink">Join</a>
         </nav>
     </header>
     </div>
